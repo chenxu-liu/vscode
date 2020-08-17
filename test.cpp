55e51d51cde215faff785ff99b9c9df75298d66f
unknown <1154106923@qq.com>
2020-08-17 23:52:24 +0800

    cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-using namespace std;
-// class shape 
+// #include <iostream>
+// using namespace std;
+// // class shape 
 // {public: 
 // virtual int area()=0;
 // }; 
@@ -19,27 +19,50 @@ using namespace std;
 //     //shape s3=r;
     
 // }
-template<typename T>
-void my_swap(T &&a, T &&b) {
-    typename remove_reference<T>::type c;
-    c = a;
-    a = b;
-    b = c;
-    return ;
-}
 // template<typename T>
-// void my_swap(T &a, T &b) {
-//     T c;
+// void my_swap(T &&a, T &&b) {
+//     typename remove_reference<T>::type c;
 //     c = a;
 //     a = b;
 //     b = c;
 //     return ;
 // }
-int main() {
-    int a = 123, b = 456;
-    cout << a << " " << b << endl;
-    my_swap(a, b);
-    cout << a << " " << b << endl;
-    my_swap(move(a), move(b));
-    cout << a << " " << b << endl;
+
+// int main() {
+//     int a = 123, b = 456;
+//     cout << a << " " << b << endl;
+//     my_swap(a, b);
+//     cout << a << " " << b << endl;
+//     my_swap(move(a), move(b));
+//     cout << a << " " << b << endl;
+// }
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+namespace haizei{
+    template<typename T>
+    struct less{
+        bool operator()(const T &a,const T &b){
+            return a<b;
+        }
+    };
+}
+int a = 0;
+
+int y(){
+    a++;
+    return 0;
+}
+
+int main(){
+    int x = 0;
+    auto inc = [&](){ x++; };
+    inc();
+    inc();
+    y();
+    cout << a << endl;
+    int arr[5]={5,6,17,3,5};
+    sort(arr,arr+5,haizei::less<int>());
+    return 0;
 }
